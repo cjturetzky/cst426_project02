bb42df1ccc705b80595775c9a8c403e0bcc1e1a8
Niko <70172259+NikoCSUMB@users.noreply.github.com>
2021-10-17 21:35:24 -0700

    Menu Toggle
    Now switches between action selection and enemy selection menus when submitting and cancelling.

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BattleManager : MonoBehaviour
+{
+    private EventSystem eventSystem;
+    public string selectedAction;
+
+    public static BattleManager Instance {private set ; get;}
+    void Start()
+    {
+        Instance = this;
+        eventSystem = this.GetComponent<EventSystem>();
+    }
+
+    public void SetAction(string action)
+    {
+        selectedAction = action;
+        eventSystem.SetSelectedGameObject(GameObject.FindGameObjectWithTag("Enemy"));
+    }
+}
--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 // Base class inherited by all Robot enemies
-public class Enemy : MonoBehaviour, ISelectHandler, IDeselectHandler
+public class Enemy : MonoBehaviour, ISelectHandler, IDeselectHandler, ISubmitHandler, ICancelHandler
 {
     public void OnSelect(BaseEventData eventData)
     {
-        Debug.Log("Highlighting " + this.gameObject.name);
         GetComponent<SpriteRenderer>().material.color = Color.blue;
     }
 
     public void OnDeselect(BaseEventData eventData)
     {
-        Debug.Log("Deselecting " + this.gameObject.name + "...");
         GetComponent<SpriteRenderer>().material.color = Color.white;
+    }
+
+    public void OnSubmit(BaseEventData eventData)
+    {
+        Debug.Log("Used " + BattleManager.Instance.selectedAction + " on " + this.gameObject.name);
+    }
+
+    public void OnCancel(BaseEventData eventData)
+    {
+        eventData.selectedObject = GameObject.Find("HackButton");
     }
 }
